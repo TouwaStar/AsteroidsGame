1a5e165a093d95eb8bee15b29c6b565dde793bca
TouwaStar <30479449+TouwaStar@users.noreply.github.com>
2018-08-25 14:32:27 +0200

    Changing to pure virtual functions

--- AsteroidsGame/GameObject.h
+++ AsteroidsGame/GameObject.h
@@ -7,7 +7,7 @@ protected:
 public:
 	float positionX, positionY; // Keep track of objects position
 	float w, h, d; // Width height and depth of the object
-	virtual void drawSelf() const {};
-	virtual void move(float frameDelta) {};
+	virtual void drawSelf() const = 0;
+	virtual void move(float frameDelta) = 0;
 	bool outOfBounds; // Indicates whether the object is out of bounds
 }; 
